ad406a8cc00ccde528fdae952097bf231cd93fdc
gaofeng222 <596642721@qq.com>
2025-12-16 16:05:20 +0800

    init

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+export default function About(){
+    return <main className="flex min-h-screen flex-col p-6">
+        <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+             <AcmeLogo />
+        </div>
+    </main>
+}
